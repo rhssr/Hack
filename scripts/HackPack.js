3ac137b12f119be5a6d7a7f17ea9e75dde255514
ObeyLordGoomy <Swaggymudkip69@gmail.com>
2018-04-29 15:39:03 -0400

    Update HackPack.js

--- scripts/HackPack.js
+++ scripts/HackPack.js
@@ -7,6 +7,16 @@
 // @grant        GM_setClipboard
 // ==/UserScript==
 (() => {
+/*
+KEYBINDS:
+P: win track
+SHIFT: noclip
+1: heli
+2: truck
+3: balloon
+0: bike
+W,A,S,D: change gravity
+*/
 GameSettings.vehiclePowerup.minTime = -999;
 GameSettings.vehiclePowerup.time = 0;
 GameSettings.vehiclePowerup.maxTime = 999;
@@ -20,9 +30,10 @@ user.admin = true;
     document.addEventListener('keydown', e => {
 		switch (e.keyCode) {
 			case 16: GameSettings.physicsSectorSize = 0; break;//shift
-            case 80: GameManager.game.currentScene.trackComplete(); break;//"p"
+            case 80: GameManager.game.currentScene.verifyComplete = ()=>{return !0;}; GameManager.game.currentScene.trackComplete(); break;//"p"
             case 49: GameManager.game.currentScene.playerManager.firstPlayer.createTempVehicle("HELI", 29970,  GameManager.game.currentScene.camera.position,GameManager.game.currentScene.playerManager.firstPlayer._baseVehicle.dir); break;//1 key
             case 50: GameManager.game.currentScene.playerManager.firstPlayer.createTempVehicle("TRUCK", 29970,  GameManager.game.currentScene.camera.position,GameManager.game.currentScene.playerManager.firstPlayer._baseVehicle.dir); break;//2
+            case 51: GameManager.game.currentScene.playerManager.firstPlayer.createTempVehicle("BALLOON", 29970,  GameManager.game.currentScene.camera.position,GameManager.game.currentScene.playerManager.firstPlayer._baseVehicle.dir); break;//3 key
             case 48: GameManager.game.currentScene.playerManager.firstPlayer.createBaseVehicle(GameManager.game.currentScene.camera.position,GameManager.game.currentScene.playerManager.firstPlayer._tempVehicle.dir,GameManager.game.currentScene.playerManager.firstPlayer._tempVehicle.masses[0].vel); break;
             case 87: GameManager.game.currentScene.playerManager.firstPlayer._baseVehicle.gravity.y = -0.3;GameManager.game.currentScene.playerManager.firstPlayer._baseVehicle.gravity.x = 0; break; //"w"
             case 65: GameManager.game.currentScene.playerManager.firstPlayer._baseVehicle.gravity.y = 0;GameManager.game.currentScene.playerManager.firstPlayer._baseVehicle.gravity.x = -0.3; break; //"a"
